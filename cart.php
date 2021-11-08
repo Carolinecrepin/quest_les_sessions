92c583a65ae3e26196d6bfe4e63a5f36c9dc7f3e
Caroline Crépin <90180533+Carolinecrepin@users.noreply.github.com>
2021-11-08 10:03:05 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,7 +1,19 @@
-<?php require 'inc/head.php'; ?>
+<?php
+require 'inc/head.php';
+require 'inc/data/products.php';
+if (empty($_SESSION['loginname'])) {
+    header('location:login.php');
+    exit();
+}
+?>
 <section class="cookies container-fluid">
     <div class="row">
-        TODO : Display shopping cart items from $_SESSION here.
+        <?php
+        $products = $_SESSION['product'];
+        foreach ($products as $key => $product) {
+            echo "<img src='assets/img/product-" . $key . ".jpg' style = 'width: 50px; margin-right: 10px' alt='" . $catalog[$key]['name'] . "'>" . $catalog[$key]['name'] . " : " . $product . "<br>";
+        }
+        ?>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
